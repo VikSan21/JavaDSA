28396e3942886781244f4590a6368b246fce0ff4
Vikram <sankhlav96@outlook.com>
2024-02-12 02:13:14 +0530

    second mathod is added in InvertedRotatedHalfPyramid.java

--- Patterns/InvertedRotatedHalfPyramid.java
+++ Patterns/InvertedRotatedHalfPyramid.java
@@ -2,20 +2,37 @@ package Patterns;
 
 public class InvertedRotatedHalfPyramid {
 
-    public static void invertedRotatedHalfPyramid(int rowcols){
+    //this is the first method
+    public static void invertedRotatedHalfPyramid_1(int rowcols){
         for( int i = 1; i <= rowcols; ++i){
+
             for(int j = 1; j <= rowcols; ++j){
                 if(j <= rowcols - i)
                     System.out.print("   ");
                 else
                     System.out.print(" * ");
             }
+
+            System.out.println();
+        }
+    }
+
+    //this is the second method
+    public static void invertedRotatedHalfPyramid_2(int rowcols){
+        for( int i = 1; i <= rowcols; ++i){
+
+            for(int space = 1; space <= rowcols - i; ++space)
+                System.out.print("   ");
+
+            for(int stars = 1; stars <= i; ++stars)
+                System.out.print(" * ");
+
             System.out.println();
         }
     }
     
     public static void main(String[] args) {
-        invertedRotatedHalfPyramid(5);
+        invertedRotatedHalfPyramid_2(5);
     }
     
 }
