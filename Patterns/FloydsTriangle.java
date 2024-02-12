fe8bdb34360c566219fe6b3d508cebd4a965c59a
Vikram <sankhlav96@outlook.com>
2024-02-12 10:18:33 +0530

    Floyd's Triangle file : removed extra characters

--- Patterns/FloydsTriangle.java
+++ Patterns/FloydsTriangle.java
@@ -20,7 +20,7 @@ public class FloydsTriangle {
 
             for(int j = 1; j <= i; ++j){
 
-                System.out.print(count++ + "");
+                System.out.print(count++);
             }
             System.out.println();
         }
