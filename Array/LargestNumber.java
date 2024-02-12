344078702bf158d66d4ba7541e74bef5493d7fd4
Vikram <sankhlav96@outlook.com>
2024-02-12 21:57:27 +0530

    LargestNumber is modified. SmallestNumber is included.

--- Array/LargestNumber.java
+++ Array/LargestNumber.java
@@ -2,22 +2,35 @@ package Array;
 
 public class LargestNumber {
 
+    //to get the largest number from the array
     public static int getLargestNumber(int[] array){
-        int LargestNumber = array[0];
-        for(int i = 1; i < array.length; ++i){
+        int largestNumber = Integer.MIN_VALUE;
+        for(int i = 0; i < array.length; ++i){
 
-            if( array[i] >= LargestNumber)
-                LargestNumber = array[i];
+            if( array[i] >= largestNumber)
+                largestNumber = array[i];
         }
-        return LargestNumber;
+        return largestNumber;
+    }
+
+    //to get the smallest number from array
+    public static int getSmallestNumber(int[] array){
+        int smallestNumber = Integer.MAX_VALUE;
+        for(int i = 0; i < array.length; ++i){
+
+            if( array[i] <= smallestNumber)
+                smallestNumber = array[i];
+        }
+        return smallestNumber;
     }
     
     public static void main(String[] args) {
-        int[] array = new int[10];
+        int[] array = new int[5];
 
         //to get the array value from the user
         LinearSearch.getArrayValue(array);
 
         System.out.println("Largest number : " + getLargestNumber(array));
+        System.out.println("Smallest number : " + getSmallestNumber(array));
     }
 }
