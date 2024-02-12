648eb51920b9305e52acff9fbe5ca32ca63541b8
Vikram <sankhlav96@outlook.com>
2024-02-12 10:17:14 +0530

    Floyd's Triangle problem file added

---
+++ Patterns/FloydsTriangle.java
@@ -0,0 +1,33 @@
+package Patterns;
+
+/*  
+  
+        Floyd's Triangle
+        
+        1
+        2 3
+        4 5 6
+        7 8 9 10
+
+  */
+
+public class FloydsTriangle {
+
+
+    public static void floydsTriangle(int rows){
+        int count = 1;
+        for(int i = 1; i <= rows; ++i){
+
+            for(int j = 1; j <= i; ++j){
+
+                System.out.print(count++ + "");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        floydsTriangle(5);
+    }
+    
+}
