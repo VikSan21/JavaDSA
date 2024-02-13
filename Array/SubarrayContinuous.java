76da7eaa2fd9b6e03b546966f7f98c426a80d077
Vikram <sankhlav96@outlook.com>
2024-02-13 21:53:21 +0530

    min and max sum of subarray is added

--- Array/SubarrayContinuous.java
+++ Array/SubarrayContinuous.java
@@ -6,6 +6,7 @@ public class SubarrayContinuous {
     
         public static void printSubArrayContinuous(int[] array){
             ArrayList<Integer> integers = new ArrayList<>();
+            int minSum = Integer.MAX_VALUE, maxSum = Integer.MIN_VALUE, sum = 0;
             for(int i = 0; i < array.length; i++){
 
                 for(int j = i; j < array.length; j++){
@@ -13,12 +14,23 @@ public class SubarrayContinuous {
                    for(int k = i; k <= j; k++){
 
                        integers.add(array[k]);
+                       sum += array[k];
+
+
                    }
                    System.out.println(integers);
                    integers.clear();
+                   if(sum >= maxSum)
+                        maxSum = sum;
+                   if(sum <= minSum)
+                        minSum = sum; 
+                   sum = 0;
                 }
                 System.out.println();
             }
+
+            System.out.println("Max : " + maxSum);
+            System.out.println("Min : " + minSum);
             int totalSubArray = array.length*(array.length + 1) / 2;
             System.out.println("Total SubArrays : " + totalSubArray);
         }
