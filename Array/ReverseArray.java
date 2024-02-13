acdd39ed053530cdc27adbb48ad39162878df344
Vikram <sankhlav96@outlook.com>
2024-02-13 20:16:32 +0530

    second method is added to ReverseArray.java

--- Array/ReverseArray.java
+++ Array/ReverseArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class ReverseArray {
     
-    public static void reverseArray(int[] array){
+    public static void reverseArrayF(int[] array){
 
         for(int i = 0; i < array.length / 2; i++ ){
             int temp = array[i];
@@ -14,11 +14,24 @@ public class ReverseArray {
 
     }
 
+    public static void reverseArrayS(int[] array){
+        int first = 0, last = array.length -1;
+
+        while(first < last){
+            int temp = array[first];
+            array[first] = array[last];
+            array[last] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] array = new int[]{21, 85, 45, 69, 78, 365, 45, 215, 85, 452};
 
-        reverseArray(array);
+        reverseArrayF(array);
 
         System.out.println(Arrays.toString(array));
     }
